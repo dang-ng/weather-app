fb7b5271aa24795813a947b0a9f7d1e78b3b8151
derek089 <nguyen.hai.dang@icloud.com>
2022-09-17 10:16:02 +0700

    1st commit

---
+++ src/components/TopButtons.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+
+function TopButtons() {
+
+  const cities = [
+    {
+      id: 1,
+      title: "Hồ Chí Minh",
+    },
+    {
+      id: 2,
+      title: "Hà Nội",
+    },
+    {
+      id: 3,
+      title: "New York",
+    },
+    {
+      id: 4,
+      title: "Tokyo",
+    },
+    {
+      id: 5,
+      title: "Paris",
+    },
+  ];
+
+
+  return <div className="flex items-center justify-around my-6">
+    {cities.map((city) =>
+      <button key={city.id} className='text-white text-lg font-medium'>{city.title}</button>
+    )}
+  </div>;
+}
+
+export default TopButtons
